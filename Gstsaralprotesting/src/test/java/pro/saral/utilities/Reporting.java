eaf8e965251ee448cddd4edb7ae0383be315ce34
Darshan <Darshan@Darshan.Relyon.com>
2022-06-02 16:54:40 +0530

    Report changes

--- Gstsaralprotesting/src/test/java/pro/saral/utilities/Reporting.java
+++ Gstsaralprotesting/src/test/java/pro/saral/utilities/Reporting.java
@@ -27,7 +27,7 @@ public class Reporting extends TestListenerAdapter
 	public void onStart(ITestContext testContext)
 	{
 		String timeStamp=new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-		String repName="Test-Report-"+".html";
+		String repName="Test-Report"+".html";
 	
 		htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir")+"/test-Output/"+repName);
 		htmlReporter.loadXMLConfig(System.getProperty("user.dir")+"/extent-config.xml");
